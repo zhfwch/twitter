b454d205ee8c58ce04bbee44c13f8eeb84079c9e
zhfwch <zhfwch@126.com>
2015-08-21 15:02:23 +0800

    chage log

--- twitters.py
+++ twitters.py
@@ -3,17 +3,20 @@ import twitter
 import requests
 import re
 
-def gettext(id, flag):
+def gettext(id, title, flag):
+	title = title.replace(' ', '_')
 	if flag:
 		page1 = requests.get('http://t.co/%s' % id)
 	else:
 		page1 = requests.get('http://pastebin.com/%s' % id)
 	t = re.findall('<textarea id=".*?" .*?>(.*?)</textarea>',page1.text,re.S)
-	f = open(id + '.txt','w')
 	if len(t) > 0:
+		f = open(title + '_' + id + '.txt','w')
 		f.write(t[0])
-	f.close()
-file = open('spider.txt', 'w')
+		f.close()
+		return True
+	return False
+file = open('pastebin.txt', 'w')
 api = twitter.Api(consumer_key='LHHL6A3TbtspuYyyZzGfZxVlX', 
 consumer_secret='z2cvLoPROQeb1YhmDUHPROgY7NN9cXRJY2ZfN5WxYDLnSoK4W2', 
 access_token_key='3318355362-p3FYuDgyIPu6DdrrWTDhhE41r740k8nw3qzj7l0', 
@@ -26,9 +29,7 @@ while len(usertimelines) > 0:
 	tweets.extend(usertimelines)
 	id1 = tweets[-1].id - 1
 flag = True
-index = 0
 for tweet in tweets:
-	index += 1
 	text = tweet.text
 	if text.find(' http://t.co/') != -1:
 		s = text.split(' http://t.co/')
@@ -42,7 +43,7 @@ for tweet in tweets:
 	id = s[1]
 	s = id.split(' #')
 	id = s[0]
-	file.write(title + '\t' + id + '\n')
-	gettext(id, flag)
-print(index)
+	pastebin = gettext(id, title, flag)
+	if pastebin:
+		file.write(title + '\t' + id + '\n')
 file.close()
